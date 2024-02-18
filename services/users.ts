7d9e4c26d122b0eaf90a1e7173cdf02ff75eab07
Abdulaziz-Mirsagatov <mirsagah@mail.uc.edu>
2024-02-18 17:21:28 -0500

    cashe no-store

--- services/users.ts
+++ services/users.ts
@@ -1,6 +1,7 @@
 export const getUser = async (id: string) => {
   const res = await fetch(`${process.env.API_URL}/api/user/${id}`, {
     method: "GET",
+    cache: "no-store",
   });
   if (!res.ok) return Promise.resolve("");
 
@@ -11,6 +12,7 @@ export const getUsers = async () => {
   const res = await fetch(`${process.env.API_URL}/api/users`, {
     method: "GET",
     next: { tags: ["users"] },
+    cache: "no-store",
   });
   if (!res.ok) return Promise.resolve([]);
 
