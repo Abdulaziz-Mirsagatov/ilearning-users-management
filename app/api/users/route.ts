5cf75098b8ad61f99247c03b6c775aee13a1f47a
Abdulaziz-Mirsagatov <mirsagah@mail.uc.edu>
2024-02-18 18:56:02 -0500

    force-dynamic added to route handlers

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest, res: NextResponse) {
   const users = await prisma.user.findMany();
 
